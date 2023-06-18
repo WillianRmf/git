47dad5d9c90351bec64ff1e55c23a4aa579279b2
WillianRmf <willian.rmf@gmail.com>
2023-06-18 19:18:32 -0300

    Este é o jogo da vaquinha que tem que atravessar a rua

---
+++ carro.js
@@ -0,0 +1,31 @@
+
+//Codigo do Carro
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [7, 2.5, 3.2, 5, 3.3, 2.2];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+function mostraCarro(){
+  for (let i =0; i < imagemCarros.length; i++){
+    image(imagemCarros[i],xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+  
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPoicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+  if(passouTodaATela(xCarros[i])){
+    xCarros[i] = 600;
+    }
+  }
+}
+function passouTodaATela(xCarro){
+  return xCarro < -50;
+}
